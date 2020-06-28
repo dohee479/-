3341e6d2594ae6bff8c43b931fbe83881c549379
dohee479 <dohee479@naver.com>
2020-06-29 03:03:08 +0900

    백준

--- BFS/2178.py
+++ BFS/2178.py
@@ -5,22 +5,21 @@ sys.stdin = open('input.txt', 'r')
 def bfs(x, y):
     dx = [0, 0, -1, 1]
     dy = [-1, 1, 0, 0]
-    stack = [(x, y)]
+    queue = [(x, y)]
     adj[x][y] = 1
-    while stack:
-        x, y = stack.pop(0)
+    while queue:
+        x, y = queue.pop(0)
         if x == N-1 and y == M-1:
-            print(adj[x][y])
-            break
+            return adj[x][y]
         for direct in range(4):
             nx = x + dx[direct]
             ny = y + dy[direct]
             if 0 <= nx < N and 0 <= ny < M and maze[nx][ny] == 1 and adj[nx][ny] == 0:
-                stack.append((nx, ny))
+                queue.append((nx, ny))
                 adj[nx][ny] = adj[x][y] + 1
 
 
 N, M = map(int, input().split())
 maze = [list(map(int, input())) for _ in range(N)]
 adj = [[0] * M for _ in range(N)]
-bfs(0, 0)
+print(bfs(0, 0))
