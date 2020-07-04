4feecc665d3f0a1da3e17b9783044c5834ea1cec
dohee479 <dohee479@naver.com>
2020-07-04 20:53:28 +0900

    백준

--- BFS/2583.py
+++ BFS/2583.py
@@ -6,16 +6,16 @@ def bfs(x, y):
     result = 1
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-    stack = [(x, y)]
+    queue = [(x, y)]
     adj[x][y] = 1
-    while stack:
-        x, y = stack.pop(0)
+    while queue:
+        x, y = queue.pop(0)
         space[x][y] = 2
         for direct in range(4):
             nx = x + dx[direct]
             ny = y + dy[direct]
             if 0 <= nx < N and 0 <= ny < M and space[nx][ny] == 0 and adj[nx][ny] == 0:
-                stack.append((nx, ny))
+                queue.append((nx, ny))
                 adj[nx][ny] = 1
                 result += 1
     return result
