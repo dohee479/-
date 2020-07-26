7b1fa7d8ab6fda3470d92b14e1e83997f83dc173
dohee479 <dohee479@naver.com>
2020-07-26 14:08:35 +0900

    백준

--- 그리디 알고리즘/1931.py
+++ 그리디 알고리즘/1931.py
@@ -4,6 +4,8 @@ sys.stdin = open('input.txt', 'r')
 
 N = int(input())
 meeting = sorted([list(map(int, sys.stdin.readline().split())) for i in range(N)], key=lambda x: (x[1], x[0]))
+# meeting = sorted(meeting, key=lambda time: time[0])
+# meeting = sorted(meeting, key=lambda time: time[1])
 print(meeting)
 
 end = 0
@@ -14,12 +16,4 @@ for m in meeting:
         cnt += 1
 print(cnt)
 
-# N = int(input())
-# meeting = [list(map(int, input().split())) for _ in range(N)]
-# meeting.sort()
-# for i in range(N-1, -1, -1):
-#     for j in range(i):
-#         if meeting[j][1] > meeting[j+1][1]:
-#             meeting[j], meeting[j + 1] = meeting[j+1], meeting[j]
-#
-# print(meeting)
+
