a8719ba7ea138905c5dc0812884219ead4ebc730
dohee479 <dohee479@naver.com>
2020-08-03 22:58:24 +0900

    백준

--- BFS/7569.py
+++ BFS/7569.py
@@ -4,15 +4,15 @@ from collections import deque
 sys.stdin = open('input.txt', 'r')
 
 def bfs():
-    dx = [-1, 1, 0, 0, 0, 0]
-    dy = [0, 0, -1, 1, 0, 0]
-    dz = [0, 0, 0, 0, -1, 1]
+    dz = [-1, 1, 0, 0, 0, 0]
+    dx = [0, 0, -1, 1, 0, 0]
+    dy = [0, 0, 0, 0, -1, 1]
     while queue:
         z, x, y = queue.popleft()
         for direct in range(6):
+            nz = z + dz[direct]
             nx = x + dx[direct]
             ny = y + dy[direct]
-            nz = z + dz[direct]
             if 0 <= nx < N and 0 <= ny < M and 0 <= nz < H and tomato[nz][nx][ny] == 0:
                 queue.append((nz, nx, ny))
                 tomato[nz][nx][ny] = tomato[z][x][y] + 1
@@ -24,21 +24,16 @@ def bfs():
                 if tomato[z][x][y] == 0:
                     return -1
                 min_day = max(min_day, tomato[z][x][y])
-                return min_day-1
+    return min_day-1
 
 
 M, N, H = map(int, input().split())
 tomato = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
 queue = deque()
-cnt = 0
 for z in range(H):
     for x in range(N):
         for y in range(M):
             if tomato[z][x][y] == 1:
                 queue.append((z, x, y))
-            else:
-                cnt += 1
-if cnt:
-    print(bfs())
-else:
-    print(0)
+print(bfs())
+
