638f0de39fdb6e495b2c60aace7fdefb85186df5
dohee479 <dohee479@naver.com>
2020-08-24 17:05:58 +0900

    백준

--- 정렬/10989.py
+++ 정렬/10989.py
@@ -1,30 +1,30 @@
 # 수 정렬하기 3
 import sys
 sys.stdin = open('input.txt', 'r')
-# input = sys.stdin.readline
+input = sys.stdin.readline
 
 
 # def counting_sort(length, max_num):
-#     counting_array = [0] * (max_num+1)
-#     result_array = [0] * length
-#
-#     for num in number:
-#         counting_array[num] += 1
-#
-#     for index in range(len(counting_array)-1):
-#         counting_array[index+1] += counting_array[index]
-#
-#     for num in number:
-#         result_array[counting_array[num]-1] = num
-#         counting_array[num] -= 1
-#     return result_array
-#
-#
-# N = int(input())
-# number = [int(input()) for _ in range(N)]
-# result = counting_sort(len(number), max(number))
-# for num in result:
-#     print(num)
+# #     counting_array = [0] * (max_num+1)
+# #     result_array = [0] * length
+# #
+# #     for num in number:
+# #         counting_array[num] += 1
+# #
+# #     for index in range(len(counting_array)-1):
+# #         counting_array[index+1] += counting_array[index]
+# #
+# #     for num in number:
+# #         result_array[counting_array[num]-1] = num
+# #         counting_array[num] -= 1
+# #     return result_array
+# #
+# #
+# # N = int(input())
+# # number = [int(input()) for _ in range(N)]
+# # result = counting_sort(len(number), max(number))
+# # for num in result:
+# #     print(num)
 
 N = int(input())
 number = [0] * 10001
---
+++ 정렬/2750.py
@@ -0,0 +1,52 @@
+# 수 정렬하기
+import sys
+sys.stdin = open('input.txt', 'r')
+
+
+def countingSort(arr, digit):
+    n = len(arr)
+    result = [0] * n
+    count = [0] * 10
+
+    for i in range(len(arr)):
+        index = int(arr[i]/digit)
+        count[(index % 10)] += 1
+
+    for i in range(9):
+        count[i + 1] += count[i]
+
+    for i in range(n-1, -1, -1):
+        index = int(arr[i]/digit)
+        result[count[(index % 10)] - 1] = arr[i]
+        count[(index % 10)] -= 1
+
+    for i in range(n):
+        arr[i] = result[i]
+
+
+def radixSort(arr):
+    maxNum = max(arr)
+    digit = 1
+    while int(maxNum/digit) > 0:
+        countingSort(arr, digit)
+        digit *= 10
+
+
+
+N = int(input())
+plus = []
+minus = []
+for i in range(N):
+    num = int(input())
+    if num >= 0:
+        plus.append(num)
+    else:
+        minus.append(abs(num))
+if plus:
+    radixSort(plus)
+if minus:
+    radixSort(minus)
+for m_num in reversed(minus):
+    print(-m_num)
+for p_num in plus:
+    print(p_num)
