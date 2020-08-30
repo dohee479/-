01d8e1af7ab9826402f051f433d4756c10d0fb4b
dohee479 <dohee479@naver.com>
2020-08-30 23:51:47 +0900

    백준

--- 정렬/2751.py
+++ 정렬/2751.py
@@ -3,41 +3,41 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 # 첫번째 radix sort
-# def counting_sort(arr, digit):
-#     n = len(arr)
-#
-#     result = [0] * n
-#     count = [0] * 10
-#
-#     for i in range(n):
-#         index = int(arr[i]/digit)
-#         count[(index % 10)] += 1
-#
-#     for i in range(9):
-#         count[i + 1] += count[i]
-#
-#     for i in range(n-1, -1, -1):
-#         index = int(arr[i]/digit)
-#         result[count[(index % 10)] - 1] = arr[i]
-#         count[(index % 10)] -= 1
-#
-#     for i in range(n):
-#         arr[i] = result[i]
-#
-#
-# def radix_sort(arr):
-#     maxNum = max(arr)
-#     digit = 1
-#     while int(maxNum/digit) > 0:
-#         counting_sort(arr, digit)
-#         digit *= 10
-#
-#
-# N = int(input())
-# arr = [int(input()) + 1000000 for _ in range(N)]
-# radix_sort(arr)
-# for num in arr:
-#     print(num - 1000000)
+def counting_sort(arr, digit):
+    n = len(arr)
+
+    result = [0] * n
+    count = [0] * 10
+
+    for i in range(n):
+        index = int(arr[i]/digit)
+        count[(index % 10)] += 1
+
+    for i in range(9):
+        count[i + 1] += count[i]
+
+    for i in range(n-1, -1, -1):
+        index = int(arr[i]/digit)
+        result[count[(index % 10)] - 1] = arr[i]
+        count[(index % 10)] -= 1
+
+    for i in range(n):
+        arr[i] = result[i]
+
+
+def radix_sort(arr):
+    maxNum = max(arr)
+    digit = 1
+    while int(maxNum/digit) > 0:
+        counting_sort(arr, digit)
+        digit *= 10
+
+
+N = int(input())
+arr = [int(input()) + 1000000 for _ in range(N)]
+radix_sort(arr)
+for num in arr:
+    print(num - 1000000)
 
 
 # 두번째 quick sort
@@ -63,26 +63,31 @@ sys.stdin = open('input.txt', 'r')
 #     print(num)
 
 
-# 세번째 quick sort (pypy3)
-# def quick_sort(arr, start, end):
-#     if end <= start:
-#         return arr
-#     pivot = arr[(start + end) // 2]
+# 세번째 quick sort
+# def quick_sort(arr):
+#     def sort(low, high):
+#         if high <= low:
+#             return
 #
-#     left = start
-#     right = end
-#     while left <= right:
-#         while arr[left] < pivot:
-#             left += 1
-#         while arr[right] > pivot:
-#             right -= 1
-#         if start <= end:
-#             arr[left], arr[right] = arr[right], arr[left]
-#             left, right = left + 1, right - 1
-#     quick_sort(arr, start, left - 1)
-#     quick_sort(arr, left, end)
+#         mid = partition(low, high)
+#         sort(low, mid - 1)
+#         sort(mid, high)
 #
+#     def partition(low, high):
+#         pivot = arr[(low + high) // 2]
+#         while low <= high:
+#             while arr[low] < pivot:
+#                 low += 1
+#             while arr[high] > pivot:
+#                 high -= 1
+#             if low <= high:
+#                 arr[low], arr[high] = arr[high], arr[low]
+#                 low, high = low + 1, high - 1
+#         return low
 #
+#     return sort(0, len(arr) - 1)
+
+
 # N = int(input())
 # arr = [int(input()) for _ in range(N)]
 # quick_sort(arr, 0, len(arr) - 1)
