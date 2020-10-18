078df608d308d97c570858e969cccfacb886dea3
dohee479 <dohee479@naver.com>
2020-10-18 23:23:57 +0900

    백준

--- 다이나믹 프로그래밍/1463.py
+++ 다이나믹 프로그래밍/1463.py
@@ -1,4 +1,14 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-N = int(input())
+
+def fibo(n):
+    numbers = [0] * (n+1)
+    numbers[1] = 1
+    for i in range(2, n+1):
+        numbers[i] = numbers[i - 1] + numbers[i - 2]
+    return numbers[n]
+
+
+n = int(input())
+print(fibo(n))
