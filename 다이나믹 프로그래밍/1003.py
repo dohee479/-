0f85ff2c3d7a21e4915922b25ab6f5d830e36b70
dohee479 <dohee479@naver.com>
2020-10-19 22:07:31 +0900

    백준

--- 다이나믹 프로그래밍/1003.py
+++ 다이나믹 프로그래밍/1003.py
@@ -3,16 +3,17 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 
-# def fibo(n):
-#     if n == 0:
-#         return '1 0'
-#     elif n == 1:
-#         return '0 1'
-#     numbers = [0] * (n + 1)
-#     numbers[1] = 1
-#     for i in range(2, n+1):
-#         numbers[i] = numbers[i - 1] + numbers[i - 2]
-#     return '{} {}'.format(numbers[n - 1], numbers[n])
+def fibo(n):
+    if n == 0:
+        return '1 0'
+    elif n == 1:
+        return '0 1'
+    numbers = [0] * (n + 1)
+    numbers[1] = 1
+    for i in range(2, n+1):
+        numbers[i] = numbers[i - 1] + numbers[i - 2]
+    return '{} {}'.format(numbers[n - 1], numbers[n])
+
 
 def fibo(n):
     if n == 0:
