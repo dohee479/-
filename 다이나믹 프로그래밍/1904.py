735a73ded388db51e0423cead42b35ceb65208be
dohee479 <dohee479@naver.com>
2020-10-21 02:31:04 +0900

    백준

--- 다이나믹 프로그래밍/1904.py
+++ 다이나믹 프로그래밍/1904.py
@@ -1,3 +1,22 @@
 # 01타일
 import sys
 sys.stdin = open('input.txt', 'r')
+
+
+def tile(n, result):
+    global cnt
+    binary = ['00', '1']
+    if len(result) == n:
+        cnt += 1
+        return
+
+    for i in binary:
+        if len(result + i) <= n:
+            tile(n, result + i)
+    return cnt % 15746
+
+
+N = int(input())
+cnt = 0
+print(tile(N, ''))
+
