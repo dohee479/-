a18c5ded2732f409b607289dbeff294e037c683b
dohee479 <dohee479@naver.com>
2020-10-22 01:03:54 +0900

    백준

--- 다이나믹 프로그래밍/1904.py
+++ 다이나믹 프로그래밍/1904.py
@@ -3,20 +3,32 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 
-def tile(n, result):
-    global cnt
-    binary = ['00', '1']
-    if len(result) == n:
-        cnt += 1
-        return
+# def tile(n, result):
+#     global cnt
+#     binary = ['00', '1']
+#     if len(result) == n:
+#         cnt += 1
+#         return
+#
+#     for i in binary:
+#         if len(result + i) <= n:
+#             tile(n, result + i)
+#     return cnt % 15746
 
-    for i in binary:
-        if len(result + i) <= n:
-            tile(n, result + i)
-    return cnt % 15746
+
+def tile(n):
+
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    count1, count2 = 1, 2
+    for _ in range(2, n+1):
+        count1, count2 = count2, count1 + count2
+    return count2 % 15
 
 
 N = int(input())
 cnt = 0
-print(tile(N, ''))
+print(tile(N))
 
