c518dbbc3c4e8502b783aa5106899a8321c14ef9
dohee479 <dohee479@naver.com>
2020-10-23 15:48:43 +0900

    백준

--- 다이나믹 프로그래밍/1149.py
+++ 다이나믹 프로그래밍/1149.py
@@ -1,13 +1,18 @@
 # RGB 거리
 import sys
+from collections import deque
 sys.stdin = open('input.txt', 'r')
 
 
+def rgb(n):
+    queue = deque([(home[0][0]), (home[0][1]), (home[0][2])])
+    for i, value in enumerate(home[n]):
 
 
-
+    return
 
 
 N = int(input())
 home = [list(map(int, input().split())) for _ in range(N)]
 memory = [[0] * N for _ in range(N)]
+print(rgb(0))
