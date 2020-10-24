999fa2e244a03648ec5f36058ec2ecb8989f3420
dohee479 <dohee479@naver.com>
2020-10-24 23:47:25 +0900

    백준

--- 다이나믹 프로그래밍/1149.py
+++ 다이나믹 프로그래밍/1149.py
@@ -1,23 +1,17 @@
 # RGB 거리
 import sys
-from collections import deque
 sys.stdin = open('input.txt', 'r')
 
 
-def rgb(n):
-    queue = deque([(0, 0, home[0][0]), (0, 1, home[0][1]), (0, 2, home[0][2])])
-    while queue:
-        x, y, color = queue.popleft()
-        if x + 1 < N and y <= N:
-            for i, value in enumerate(home[x + 1]):
-                if y != i and color + home[x + 1][i] <= memory[x + 1][i]:
-                    memory[x + 1][i] = color + home[x + 1][i]
-                    queue.append((x + 1, i, color + home[x + 1][i]))
-    return min(memory[N - 1])
+def rgb():
+    for i in range(1, N):
+        home[i][0] = home[i][0] + min(home[i - 1][1], home[i - 1][2])
+        home[i][1] = home[i][1] + min(home[i - 1][0], home[i - 1][2])
+        home[i][2] = home[i][2] + min(home[i - 1][0], home[i - 1][1])
+    return min(home[N-1])
 
 
 N = int(input())
 home = [list(map(int, input().split())) for _ in range(N)]
-INF = float('inf')
-memory = [[INF] * N for _ in range(N)]
-print(rgb(0))
+print(rgb())
+
