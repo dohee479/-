5bd016312142f293eac28f27849ffea1fac83bdd
dohee479 <dohee479@naver.com>
2020-10-24 16:28:03 +0900

    백준

--- 다이나믹 프로그래밍/1149.py
+++ 다이나믹 프로그래밍/1149.py
@@ -5,14 +5,19 @@ sys.stdin = open('input.txt', 'r')
 
 
 def rgb(n):
-    queue = deque([(home[0][0]), (home[0][1]), (home[0][2])])
-    for i, value in enumerate(home[n]):
-
-
-    return
+    queue = deque([(0, 0, home[0][0]), (0, 1, home[0][1]), (0, 2, home[0][2])])
+    while queue:
+        x, y, color = queue.popleft()
+        if x + 1 < N and y <= N:
+            for i, value in enumerate(home[x + 1]):
+                if y != i and color + home[x + 1][i] <= memory[x + 1][i]:
+                    memory[x + 1][i] = color + home[x + 1][i]
+                    queue.append((x + 1, i, color + home[x + 1][i]))
+    return min(memory[N - 1])
 
 
 N = int(input())
 home = [list(map(int, input().split())) for _ in range(N)]
-memory = [[0] * N for _ in range(N)]
+INF = float('inf')
+memory = [[INF] * N for _ in range(N)]
 print(rgb(0))
