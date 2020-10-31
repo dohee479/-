26ec3afebf84bba782ac763f07be6adb3888aa7b
dohee479 <dohee479@naver.com>
2020-10-31 16:57:27 +0900

    백준

---
+++ 수학/2292.py
@@ -0,0 +1,18 @@
+# 벌집
+import sys
+sys.stdin = open('input.txt', 'r')
+
+
+def bee(n):
+    cnt = 1
+    num = 1
+    if n == 1:
+        return 1
+    while num < n:
+        num += cnt * 6
+        cnt += 1
+    return cnt
+
+
+N = int(input())
+print(bee(N))
