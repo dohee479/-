7fbac893d689f83674adc8461a5963bccdc3888a
dohee479 <dohee479@naver.com>
2020-11-02 23:25:45 +0900

    백준

---
+++ 재귀/10870.py
@@ -0,0 +1,15 @@
+# 피보나치 수 5
+import sys
+sys.stidin = open('input.txt', 'r')
+
+
+def fibo(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibo(n - 1) + fibo(n - 2)
+
+
+N = int(input())
+print(fibo(N))
