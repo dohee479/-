168744dcf3072587a981e9dc2c63b0bff75cda0e
dohee479 <dohee479@naver.com>
2020-11-07 22:10:09 +0900

    백준

--- 문자열/1316.py
+++ 문자열/1316.py
@@ -2,16 +2,28 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-alpha = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
+
+def check():
+    alpha = {'A': -1, 'B': -1, 'C': -1, 'D': -1, 'E': -1, 'F': -1, 'G': -1, 'H': -1, 'I': -1, 'J': -1, 'K': -1, 'L': -1,
+             'M': -1, 'N': -1, 'O': -1, 'P': -1, 'Q': -1, 'R': -1, 'S': -1, 'T': -1, 'U': -1, 'V': -1, 'W': -1, 'X': -1,
+             'Y': -1, 'Z': -1}
+
+    for i, value in enumerate(word):
+        if alpha[value] != -1:
+            if abs(i - alpha[value]) > 1:
+                return 0
+            else:
+                alpha[value] = i
+        else:
+            alpha[value] = i
+    return 1
+
+
 N = int(input())
 cnt = 0
 for _ in range(N):
     word = input().upper()
-    for i in range(len(word)):
-        for j in range(len(word)):
-            if word[i] == word[j]:
-                if abs(i - j) > 1:
-                    break
+    cnt += check()
 print(cnt)
 
 
