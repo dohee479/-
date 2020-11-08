6cf8301147748b5950f1cc19845f7bf32e0850c0
dohee479 <dohee479@naver.com>
2020-11-08 22:42:33 +0900

    백준

--- 문자열/1316.py
+++ 문자열/1316.py
@@ -3,27 +3,36 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 
-def check():
-    alpha = {'A': -1, 'B': -1, 'C': -1, 'D': -1, 'E': -1, 'F': -1, 'G': -1, 'H': -1, 'I': -1, 'J': -1, 'K': -1, 'L': -1,
-             'M': -1, 'N': -1, 'O': -1, 'P': -1, 'Q': -1, 'R': -1, 'S': -1, 'T': -1, 'U': -1, 'V': -1, 'W': -1, 'X': -1,
-             'Y': -1, 'Z': -1}
+# def check():
+#     alpha = {'A': -1, 'B': -1, 'C': -1, 'D': -1, 'E': -1, 'F': -1, 'G': -1, 'H': -1, 'I': -1, 'J': -1, 'K': -1, 'L': -1,
+#              'M': -1, 'N': -1, 'O': -1, 'P': -1, 'Q': -1, 'R': -1, 'S': -1, 'T': -1, 'U': -1, 'V': -1, 'W': -1, 'X': -1,
+#              'Y': -1, 'Z': -1}
+#
+#     for i, value in enumerate(word):
+#         if alpha[value] != -1:
+#             if abs(i - alpha[value]) > 1:
+#                 return 0
+#             else:
+#                 alpha[value] = i
+#         else:
+#             alpha[value] = i
+#     return 1
+#
+#
+# N = int(input())
+# cnt = 0
+# for _ in range(N):
+#     word = input().upper()
+#     cnt += check()
+# print(cnt)
 
-    for i, value in enumerate(word):
-        if alpha[value] != -1:
-            if abs(i - alpha[value]) > 1:
-                return 0
-            else:
-                alpha[value] = i
-        else:
-            alpha[value] = i
-    return 1
-
-
-N = int(input())
-cnt = 0
-for _ in range(N):
-    word = input().upper()
-    cnt += check()
-print(cnt)
+result = 0
+for i in range(int(input())):
+    word = input()
+    print(list(word))
+    print(sorted(word, key=word.find))
+#     if list(word) == sorted(word, key=word.find):
+#         result += 1
+# print(result)
 
 
--- 수학/1193.py
+++ 수학/1193.py
@@ -4,8 +4,15 @@ sys.stdin = open('input.txt', 'r')
 
 
 def fraction(n):
-
-    return
+    num = 0
+    cnt = 1
+    while num < n:
+        num += cnt
+        cnt += 1
+    if (cnt - 1) % 2:
+        return '{}/{}'.format(1+num-n, cnt-1-(num-n))
+    else:
+        return '{}/{}'.format(cnt-1-(num-n), 1+num-n)
 
 
 X = int(input())
