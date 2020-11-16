dfc8573797c36b06b267d04e1bf31b521dd4a8dd
dohee479 <dohee479@naver.com>
2020-11-17 01:56:24 +0900

    백준

--- 수학/10250.py
+++ 수학/10250.py
@@ -2,6 +2,20 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
+
+def hotel(h, w, n):
+    layer = (n-1) % h + 1
+    q, r = divmod(n, h)
+    if r:
+        room = q + 1
+    else:
+        room = q
+    if room < 10:
+        return '{}0{}'.format(layer, room)
+    return '{}{}'.format(layer, room)
+
+
 T = int(input())
 for t in range(T):
-    H, W, N =  map(int, input().split())
+    H, W, N = map(int, input().split())
+    print(hotel(H, W, N))
