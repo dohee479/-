5484aa83ee262f3fe5a1aec6af19c882601a3390
dohee479 <dohee479@naver.com>
2020-11-23 18:43:44 +0900

    백준

---
+++ 수학/2775.py
@@ -0,0 +1,13 @@
+# 부녀회장이 될테야
+import sys
+sys.stdin = open('input.txt', 'r')
+
+T = int(input())
+for t in range(T):
+    k = int(input())
+    n = int(input())
+    apart = [i for i in range(1, n+1)]
+    for i in range(k):
+        for j in range(1, n):
+            apart[j] += apart[j-1]
+    print(apart[n-1])
