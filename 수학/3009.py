f7409d7e7c983dfa120db9824d279f2909c2a58b
dohee479 <dohee479@naver.com>
2020-11-29 15:30:21 +0900

    백준

--- 수학/3009.py
+++ 수학/3009.py
@@ -2,10 +2,16 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-X = dict()
-Y = dict()
+X = [0] * 1001
+Y = [0] * 1001
 for _ in range(3):
-    x, y = input().split()
-    X.update(x)
-    Y.update(y)
-print(X, Y)
+    x, y = map(int, input().split())
+    X[x] += 1
+    Y[y] += 1
+for i, value in enumerate(X):
+    if value == 1:
+        print(i, end=' ')
+for i, value in enumerate(Y):
+    if value == 1:
+        print(i)
+
