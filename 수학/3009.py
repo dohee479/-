4716bd6620c003494e0428b7f55b9586035402da
dohee479 <dohee479@naver.com>
2020-11-30 16:17:22 +0900

    백준

--- 수학/3009.py
+++ 수학/3009.py
@@ -2,16 +2,26 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-X = [0] * 1001
-Y = [0] * 1001
+# X = [0] * 1001
+# Y = [0] * 1001
+# for _ in range(3):
+#     x, y = map(int, input().split())
+#     X[x] += 1
+#     Y[y] += 1
+# for i, value in enumerate(X):
+#     if value == 1:
+#         print(i, end=' ')
+# for i, value in enumerate(Y):
+#     if value == 1:
+#         print(i)
+#
+# x=y=0
+# exec("a,b=map(int,input().split());x^=a;y^=b;"*3)
+# print(x,y)
+
+i, j = 0, 0
 for _ in range(3):
     x, y = map(int, input().split())
-    X[x] += 1
-    Y[y] += 1
-for i, value in enumerate(X):
-    if value == 1:
-        print(i, end=' ')
-for i, value in enumerate(Y):
-    if value == 1:
-        print(i)
-
+    i ^= x
+    j ^= y
+print(i, j)
