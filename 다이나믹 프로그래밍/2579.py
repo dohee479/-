720ce60e4c35f6aecde7f74c4fe1cc1770ee2686
dohee479 <dohee479@naver.com>
2020-12-02 11:51:00 +0900

    백준

--- 다이나믹 프로그래밍/2579.py
+++ 다이나믹 프로그래밍/2579.py
@@ -3,11 +3,22 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 
-def dp():
-    for i, value in enumerate(stairs):
-        pass
-    return
+def dp(n):
+    result = [0] * n
+    result[0] = stairs[0]
+    if n == 1:
+        return result[-1]
+    result[1] = stairs[0] + stairs[1]
+    if n == 2:
+        return result[-1]
+    result[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2])
+    if result == 3:
+        return result[-1]
+    for i in range(3, n):
+        result[i] = max(stairs[i] + stairs[i - 1] + result[i - 3], stairs[i] + result[i - 2])
+    return result[-1]
 
 
 N = int(input())
 stairs = [int(input()) for _ in range(N)]
+print(dp(N))
