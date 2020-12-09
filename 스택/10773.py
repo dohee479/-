737a52366a194cad609f8531e72687eac915c14b
dohee479 <dohee479@naver.com>
2020-12-09 22:27:35 +0900

    백준

---
+++ 스택/10773.py
@@ -0,0 +1,13 @@
+# 제로
+import sys
+sys.stdin = open('input.txt', 'r')
+
+K = int(input())
+stack = []
+for _ in range(K):
+    N = int(input())
+    if N:
+        stack.append(N)
+    else:
+        stack.pop()
+print(sum(stack))
