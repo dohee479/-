5a3a5978c7eaf5744e6ba2779dbce66bafa8d9e1
dohee479 <dohee479@naver.com>
2020-12-19 15:59:47 +0900

    백준

---
+++ 수학/5068.py
@@ -0,0 +1,15 @@
+# 배수와 약수
+import sys
+sys.stdin = open('input.txt', 'r')
+
+while True:
+    num1, num2 = map(int, input().split())
+    if not num1 and not num2:
+        break
+    else:
+        if num1 >= num2 and num1 % num2 == 0:
+            print('multiple')
+        elif num1 < num2 and num2 % num1 == 0:
+            print('factor')
+        else:
+            print('neither')
