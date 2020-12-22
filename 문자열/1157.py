43acba3c6af0a7117ef51fe261347fba90d925d1
dohee479 <dohee479@naver.com>
2020-12-23 06:18:49 +0900

    단어 공부

--- 문자열/1157.py
+++ 문자열/1157.py
@@ -12,6 +12,7 @@ if count_text.count(max(count_text)) > 1:
 else:
     print(compare[count_text.index(max(count_text))])
 
+
 text = input().upper()
 alpha = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
 for i in text:
@@ -28,6 +29,7 @@ if len(answer) > 1:
     print('?')
 else:
     print(answer)
+
 
 text = input().upper()
 alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
