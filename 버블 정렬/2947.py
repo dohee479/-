8df30b3f90b5f2423362033488729e2769a58e28
dohee479 <dohee479@naver.com>
2020-12-23 08:57:34 +0900

    나무 조각

--- 버블 정렬/2947.py
+++ 버블 정렬/2947.py
@@ -5,8 +5,8 @@ sys.stdin = open('input.txt', 'r')
 num = list(map(int, input().split()))
 for i in range(len(num)-1, 0, -1):
     for j in range(0, i):
-        if num[j] > num[j+1]:
-            num[j], num[j+1] = num[j+1], num[j]
+        if num[j] > num[j + 1]:
+            num[j], num[j + 1] = num[j + 1], num[j]
             for k in num:
                 print(k, end=' ')
             print()
