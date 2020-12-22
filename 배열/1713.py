5ba8466a9fec8eb02148e390541aa44f3b01a86d
dohee479 <dohee479@naver.com>
2020-12-23 06:40:44 +0900

    후보 추천하기

--- 배열/1713.py
+++ 배열/1713.py
@@ -40,28 +40,29 @@ result.sort()
 for i in result:
     print(i, end=' ')
 
-# N = int(input())
-# V = int(input())
-# S = list(map(int, input().split()))
-# voted = [0 for _ in range(101)]
-# ans = []
-#
-# for s in S:
-#     mini = 1001
-#     if voted[s]:
-#         voted[s] += 1
-#     else:
-#         ans.append(s)
-#         voted[s] = 1
-#
-#     if len(ans) > N:
-#         for idx in range(0,len(ans) - 1):
-#             if voted[ans[idx]] < mini:
-#                 mini = voted[ans[idx]]
-#                 a = ans[idx]
-#         ans.remove(a)
-#         voted[a] = 0
-#
-# for answer in sorted(ans):
-#     print(answer, end=" ")
+
+N = int(input())
+V = int(input())
+S = list(map(int, input().split()))
+voted = [0 for _ in range(101)]
+ans = []
+
+for s in S:
+    mini = 1001
+    if voted[s]:
+        voted[s] += 1
+    else:
+        ans.append(s)
+        voted[s] = 1
+
+    if len(ans) > N:
+        for idx in range(0,len(ans) - 1):
+            if voted[ans[idx]] < mini:
+                mini = voted[ans[idx]]
+                a = ans[idx]
+        ans.remove(a)
+        voted[a] = 0
+
+for answer in sorted(ans):
+    print(answer, end=" ")
 
