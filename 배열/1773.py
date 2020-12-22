5745aa5ff46a75c1cfb76db819d019fcbc619da5
dohee479 <dohee479@naver.com>
2020-12-23 06:56:21 +0900

    폭죽쇼

--- 배열/1773.py
+++ 배열/1773.py
@@ -1,17 +1,6 @@
 # 폭죽쇼
 import sys
-
 sys.stdin = open('input.txt', 'r')
-# N, C = map(int, input().split())
-# Fire = []
-# for n in range(N):
-#     num = int(input())
-#     result = 0
-#     while result < C:
-#         result += num
-#         if result <= C and result not in Fire:
-#             Fire.append(result)
-# print(len(Fire))
 
 N, C = map(int, input().split())
 Fire = set()
@@ -25,22 +14,3 @@ for n in range(N):
         Fire.add(i)
         cnt = len(Fire)
 print(cnt)
-
-
-# Firework = []
-# for number in Fire:
-#     if number not in Firework:
-#         Firework.append(number)
-# print(len(Firework))
-
-
-# N, C = map(int, input().split())
-# check = [False]*(C+1)
-# ans = 0
-# for _ in range(N):
-#     n = int(input())
-#     for i in range(n, C+1, n):
-#         if not check[i]:
-#             check[i] = True
-#             ans += 1
-# print(ans)
