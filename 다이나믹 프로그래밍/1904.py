a606e8d10f6f5ee8a779cb8f7032ff1b4626a7b5
dohee479 <dohee479@naver.com>
2020-12-23 05:54:51 +0900

    01타일

--- 다이나믹 프로그래밍/1904.py
+++ 다이나믹 프로그래밍/1904.py
@@ -2,18 +2,19 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
+
 # 메모리 초과
-# def tile(n, result):
-#     global cnt
-#     binary = ['00', '1']
-#     if len(result) == n:
-#         cnt += 1
-#         return
-#
-#     for i in binary:
-#         if len(result + i) <= n:
-#             tile(n, result + i)
-#     return cnt % 15746
+def tile(n, result):
+    global cnt
+    binary = ['00', '1']
+    if len(result) == n:
+        cnt += 1
+        return
+
+    for i in binary:
+        if len(result + i) <= n:
+            tile(n, result + i)
+    return cnt % 15746
 
 
 def tile(n):
