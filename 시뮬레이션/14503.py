bd56dd422a815deda6d2c9ee150f9f2e0e679cc4
dohee479 <dohee479@naver.com>
2020-12-23 10:39:31 +0900

    로봇 청소기

--- 시뮬레이션/14503.py
+++ 시뮬레이션/14503.py
@@ -5,7 +5,6 @@ sys.stdin = open('input.txt', 'r')
 
 def clean(x, y, dr):
     count = 0
-    cnt = 1
     while True:
         dx = direct[dr][0]
         dy = direct[dr][1]
@@ -19,7 +18,6 @@ def clean(x, y, dr):
             y = ny
             count = 0
             if room[nx][ny] == 1:
-                print(cnt)
                 break
             continue
         else:
@@ -30,7 +28,6 @@ def clean(x, y, dr):
                 dr = (dr+3) % 4  # 다음 방향으로 바꿔준다.
                 x = nx
                 y = ny
-                cnt += 1
                 count = 0
             else:
                 dr = (dr+3) % 4
@@ -42,45 +39,10 @@ r, c, d = map(int, input().split())
 room = [list(map(int, input().split())) for _ in range(N)]
 direct = {0: [0, -1, 1, 0], 1: [-1, 0, 0, -1], 2: [0, 1, -1, 0], 3: [1, 0, 0, 1]} # 북 동 남 서 (왼쪽으로 돌아야하는 방향)
 clean(r, c, d)
+cnt = 0
+for i in range(N):
+    for j in range(M):
+        if room[i][j] == 2:
+            cnt += 1
+print(cnt)
 
-
-# cnt = 0
-# for i in range(N):
-#     for j in range(M):
-#         if room[i][j] == 2:
-#             cnt += 1
-# print(cnt)
-
-
-def clean(x, y, dr):
-    count = 0
-    cnt = 1
-    while True:
-        dx = direct[dr][0]
-        dy = direct[dr][1]
-        dx2 = direct[dr][2]
-        dy2 = direct[dr][3]
-        room[x][y] = 2  # 청소를 한다.
-        if count == 4:
-            nx = x + dx2
-            ny = y + dy2
-            x = nx
-            y = ny
-            count = 0
-            if room[nx][ny] == 1:
-                print(cnt)
-                break
-            continue
-        else:
-            nx = x + dx
-            ny = y + dy
-        if 0 <= nx < N and 0 <= ny < M:
-            if room[nx][ny] == 0:  # 탐색한 곳이 청소를 안한 곳이면
-                dr = (dr+3) % 4  # 다음 방향으로 바꿔준다.
-                x = nx
-                y = ny
-                cnt += 1
-                count = 0
-            else:
-                dr = (dr+3) % 4
-                count += 1
