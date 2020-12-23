235ef65b0cb088e30a9e6b61512582c76abadd69
dohee479 <dohee479@naver.com>
2020-12-23 10:08:39 +0900

    소수 찾기

--- 수학/1978.py
+++ 수학/1978.py
@@ -6,6 +6,7 @@ sys.stdin = open('input.txt', 'r')
 def prime(n):
     if n == 1:
         return 0
+    # 제곱근의 이유: 어떤 수의 약수는 무조건 제곱근의 범위에 존재한다.
     for i in range(2, int(n ** 0.5) + 1):
         if not (n % i):
             return 0
