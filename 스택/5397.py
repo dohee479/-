72faf66d15448a64932fa41e2d958ba36f5a8172
dohee479 <dohee479@naver.com>
2020-12-23 10:28:03 +0900

    키로거

--- 스택/5397.py
+++ 스택/5397.py
@@ -3,25 +3,26 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 # 시간초과
-# T = int(input())
-# for t in range(T):
-#     word = input()
-#     result = []
-#     stack = []
-#     for w in word:
-#         if w == '<':
-#             if result:
-#                 stack.insert(0, result.pop())
-#         elif w == '>':
-#             if stack:
-#                 result.append(stack.pop(0))
-#         elif w == '-':
-#             if result:
-#                 result.pop()
-#         else:
-#             result.append(w)
-#     result.extend(stack)
-#     print(''.join(result))
+T = int(input())
+for t in range(T):
+    word = input()
+    result = []
+    stack = []
+    for w in word:
+        if w == '<':
+            if result:
+                stack.insert(0, result.pop())
+        elif w == '>':
+            if stack:
+                result.append(stack.pop(0))
+        elif w == '-':
+            if result:
+                result.pop()
+        else:
+            result.append(w)
+    result.extend(stack)
+    print(''.join(result))
+
 
 T = int(input())
 for t in range(T):
