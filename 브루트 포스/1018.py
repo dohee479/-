dbd5c985430903d9cad59bebdf547e81b9307fc7
dohee479 <dohee479@naver.com>
2020-12-23 09:00:43 +0900

    체스판 다시 칠하기

--- 브루트 포스/1018.py
+++ 브루트 포스/1018.py
@@ -5,11 +5,11 @@ sys.stdin = open('input.txt', 'r')
 N, M = map(int, input().split())
 chess = [list(input()) for _ in range(N)]
 result = float('inf')
-for i in range(N-7):
-    for j in range(M-7):
+for i in range(N - 7):
+    for j in range(M - 7):
         start_w, start_b = 0, 0
-        for k in range(i, i+8):
-            for l in range(j, j+8):
+        for k in range(i, i + 8):
+            for l in range(j, j + 8):
                 if (k + l - i - j) % 2:
                     if chess[k][l] == 'W':
                         start_w += 1
