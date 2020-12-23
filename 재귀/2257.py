c5497ddf6ec3b8357b6981af1e62186df97b4c1c
dohee479 <dohee479@naver.com>
2020-12-23 10:42:13 +0900

    화학식량

--- 재귀/2257.py
+++ 재귀/2257.py
@@ -29,46 +29,5 @@ print(sum(stack))
 
 
 
-# def cal(i, result):
-#     global ans
-#     global tmp
-#     if i == -1:
-#         return 0
-#
-#     if chemical[i] == 'H':
-#         if result != 0:
-#             ans += result
-#         cal(i-1, 1)
-#
-#     if chemical[i] == 'C':
-#         if result != 0:
-#             ans += result
-#         cal(i-1, 12)
-#
-#     if chemical[i] == 'O':
-#         if result != 0:
-#             ans += result
-#         cal(i-1, 16)
-#
-#     elif '1' <= chemical <= '9':
-#         ans += (result*(chemical[i]-'0'))
-#         cal(i-1, 0)
-#
-#     elif chemical[i] == '(':
-#         ans += result
-#         tmp = ans
-#         cal(i-1, 0)
-#
-#     elif chemical[i] == ')':
-#         t2 = ans + result
-#         ans = tmp
-#         cal(i-1, t2-tmp)
-#
-#
-# chemical = input()
-# chem = {'H': 1, 'C': 12, 'O': 16}
-# ans = 0
-# tmp = 0
-# cal(len(chemical)-1, 0)
 
 
