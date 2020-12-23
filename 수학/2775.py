d5750907e2272dbba598bbb66571fd15c969ef3c
dohee479 <dohee479@naver.com>
2020-12-23 10:10:42 +0900

    부녀회장이 될테야

--- 수학/2775.py
+++ 수학/2775.py
@@ -9,5 +9,5 @@ for t in range(T):
     apart = [i for i in range(1, n+1)]
     for i in range(k):
         for j in range(1, n):
-            apart[j] += apart[j-1]
+            apart[j] += apart[j - 1]
     print(apart[n-1])
