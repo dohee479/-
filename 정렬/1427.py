81ff5cb9bd36b18c50f323f8abf0aa80954e31e8
dohee479 <dohee479@naver.com>
2020-12-23 10:46:59 +0900

    소트인사이드

--- 정렬/1427.py
+++ 정렬/1427.py
@@ -5,10 +5,3 @@ sys.stdin = open('input.txt', 'r')
 N = list(map(int, input()))
 result = sorted(N, reverse=True)
 print(''.join(map(str, result)))
-
-# N.sort()
-# N.reverse()
-# print(''.join(map(str, N)))
-#
-#
-# print("".join(sorted(input(), reverse=True)))
