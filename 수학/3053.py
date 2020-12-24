7ecda41ed319c2ca497c126c3d39a18adc11e5a0
dohee479 <dohee479@naver.com>
2020-12-24 18:23:29 +0900

    택시 기하학

--- 수학/3053.py
+++ 수학/3053.py
@@ -3,5 +3,7 @@ import sys, math
 sys.stdin = open('input.txt', 'r')
 
 R = int(input())
+# 유클리드 기하학에서의 원의 넓이
 print("{0:.6f}".format(math.pi * (R ** 2)))
+# 택시 기하학에서의 원의 넓이, 택시가 가는 경로를 취하는 것: 거리, 원: 마름모 꼴의 정사각형 
 print("{0:.6f}".format(2 * (R ** 2)))
