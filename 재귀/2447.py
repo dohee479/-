66a13091509cb2739800ab33d27aa6afddae8484
dohee479 <dohee479@naver.com>
2021-01-12 21:09:10 +0900

    별 찍기 - 10

--- 재귀/2447.py
+++ 재귀/2447.py
@@ -2,23 +2,52 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-# 시간초과
-def star(x, y, num):
-    if (x // num) % 3 == 1 and (y // num) % 3 == 1:
-        print(' ', end='')
-    else:
-        if num // 3 == 0:
-            print('*', end='')
-        else:
-            star(x, y, num // 3)
+
+def concatenate(r1, r2):
+    return [''.join(x) for x in zip(r1, r2, r1)]
+
+
+def star10(n):
+    if n == 1:
+        return ['*']
+    n //= 3
+    x = star10(n)
+    a = concatenate(x, x)
+    b = concatenate(x, [' ' * n] * n)
+
+    return a + b + a
+
+
+print('\n'.join(star10(int(input()))))
+
+
+# 별 찍는 재귀 함수
+def draw_star(n):
+    if n == 3:
+        Map[0][:3] = Map[2][:3] = [1] * 3
+        Map[1][:3] = [1, 0, 1]
+        return
+
+    a = n // 3
+    draw_star(n // 3)
+    for i in range(3):
+        for j in range(3):
+            if i == 1 and j == 1:
+                continue
+            for k in range(a):
+                Map[a * i + k][a * j:a * (j + 1)] = Map[k][:a]  # 핵심 아이디어
 
 
 N = int(input())
-for i in range(N):
-    for j in range(N):
-        star(i, j, N)
+Map = [[0 for i in range(N)] for i in range(N)]
+draw_star(N)
+for i in Map:
+    for j in i:
+        if j:
+            print('*', end='')
+        else:
+            print(' ', end='')
     print()
-
 
 
 
