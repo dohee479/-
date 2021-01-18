cf1bc4dcaed5f96f3dd9981363ee60d994a6eb8b
dohee479 <dohee479@naver.com>
2021-01-18 18:39:55 +0900

    스도쿠

--- 백트래킹/2580.py
+++ 백트래킹/2580.py
@@ -3,88 +3,51 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 
-# 실패 
-def backtrack(idx):
-    if idx == 9:
-        return
-    for j in range(9):
-        if not sudoku[idx][j]:
-            for k in range(1, 10):
-                if not sudoku[idx][j] and k not in sudoku[idx]:
-                    sudoku[idx][j] = k
-                    if not vertical(idx, j, k) or not square(idx, j, k):
-                        sudoku[idx][j] = 0
-            backtrack(idx + 1)
-
-
-def vertical(row, col, k):
-    for i in range(9):
-        if i != row and sudoku[i][col] == k:
-            return False
-    else:
-        return True
-
-
-def square(row, col, num):
-    if 0 <= row < 3:
-        if 0 <= col < 3:
-            for j in range(3):
-                for k in range(3):
-                    if j != row and k != col and sudoku[j][k] == num:
-                        return False
-
-        elif 3 <= col < 6:
-            for j in range(3):
-                for k in range(3, 6):
-                    if j != row and k != col and sudoku[j][k] == num:
-                        return False
-
-        elif 6 <= col < 9:
-            for j in range(3):
-                for k in range(6, 9):
-                    if j != row and k != col and sudoku[j][k] == num:
-                        return False
-
-    elif 3 <= row < 6:
-        if 0 <= col < 3:
-            for j in range(3, 6):
-                for k in range(3):
-                    if j != row and k != col and sudoku[j][k] == num:
-                        return False
-
-        elif 3 <= col < 6:
-            for j in range(3, 6):
-                for k in range(3, 6):
-                    if j != row and k != col and sudoku[j][k] == num:
-                        return False
-
-        elif 6 <= col < 9:
-            for j in range(3, 6):
-                for k in range(6, 9):
-                    if j != row and k != col and sudoku[j][k] == num:
-                        return False
-
-    elif 6 <= row < 9:
-        if 0 <= col < 3:
-            for j in range(6, 9):
-                for k in range(3):
-                    if j != row and k != col and sudoku[j][k] == num:
-                        return False
-
-        elif 3 <= col < 6:
-            for j in range(6, 9):
-                for k in range(3, 6):
-                    if j != row and k != col and sudoku[j][k] == num:
-                        return False
-
-        elif 6 <= col < 9:
-            for j in range(6, 9):
-                for k in range(6, 9):
-                    if j != row and k != col and sudoku[j][k] == num:
-                        return False
+# 가로 체크
+def horizontal(x, val):
+    if val in sudoku[x]:
+        return False
     return True
 
 
+# 세로 체크
+def vertical(y, val):
+    for i in range(9):
+        if val == sudoku[i][y]:
+            return False
+    return True
+
+
+# 3x3 체크
+def square(x, y, val):
+    nx = x//3 * 3
+    ny = y//3 * 3
+    for i in range(3):
+        for j in range(3):
+            if val == sudoku[nx+i][ny+j]:
+                return False
+    return True
+
+
+def backtrack(index):
+    if index == len(zeros):
+        for row in sudoku:
+            for n in row:
+                print(n, end=" ")
+            print()
+        sys.exit(0)
+    else:
+        for i in range(1, 10):
+            nx = zeros[index][0]
+            ny = zeros[index][1]
+
+            # 세로, 가로, 3x3에 내가 대체하려고하는 숫자가 존재하는지 확인
+            if horizontal(nx, i) and vertical(ny, i) and square(nx, ny, i):
+                sudoku[nx][ny] = i
+                backtrack(index+1)
+                sudoku[nx][ny] = 0
+
+
 sudoku = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(9)]
+zeros = [(i, j) for i in range(9) for j in range(9) if sudoku[i][j] == 0]
 backtrack(0)
-print('\n'.join([' '.join([str(j) for j in i]) for i in sudoku]))
