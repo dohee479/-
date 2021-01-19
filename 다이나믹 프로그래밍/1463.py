73ca836a0db3e4c8704895ce092fd3192d857cfa
dohee479 <dohee479@naver.com>
2021-01-19 23:23:57 +0900

    1로 만들기

--- 다이나믹 프로그래밍/1463.py
+++ 다이나믹 프로그래밍/1463.py
@@ -25,7 +25,7 @@ print(dp(N))
 
 
 # dp
-# 점화식 dp[n] = dp[n// 3] + 1, dp[n] = dp[n // 2] + 1, dp[n] = dp[n - 1] + 1
+# 점화식 dp[n] = dp[n // 3] + 1, dp[n] = dp[n // 2] + 1, dp[n] = dp[n - 1] + 1
 N = int(input())
 dp = [0 for _ in range(N + 1)]
 for i in range(2, N + 1):
