2256384bbe1e3fb4329811b1c2e4188aeaaaf275
dohee479 <dohee479@naver.com>
2021-01-19 23:23:32 +0900

    1로 만들기

--- 다이나믹 프로그래밍/1463.py
+++ 다이나믹 프로그래밍/1463.py
@@ -4,7 +4,7 @@ from collections import deque
 sys.stdin = open('input.txt', 'r')
 
 
-# bfs와 메모이제이션 이용
+# bfs
 def dp(n):
     memo = set()
     queue = deque([(n, 0)])
@@ -24,4 +24,33 @@ N = int(input())
 print(dp(N))
 
 
-# 
+# dp
+# 점화식 dp[n] = dp[n// 3] + 1, dp[n] = dp[n // 2] + 1, dp[n] = dp[n - 1] + 1
+N = int(input())
+dp = [0 for _ in range(N + 1)]
+for i in range(2, N + 1):
+    dp[i] = dp[i - 1] + 1
+    # 2로 나누었을 때, -1 한 경우보다 작을 경우
+    if i % 2 == 0 and dp[i] > dp[i // 2] + 1:
+        dp[i] = dp[i // 2] + 1
+    # 3로 나누었을 때, -1 or 2로 나눈 경우보다 작을 경우
+    if i % 3 == 0 and dp[i] > dp[i // 3] + 1:
+        dp[i] = dp[i // 3] + 1
+
+print(dp[N])
+
+
+# dp
+def dp(n):
+    if n in memo:
+        return memo[n]
+    # 나머지를 더해준 이유 짐작: 7의 경우 2, 3으로 나누어 지지 않으므로 -1를 무조건 해줘야한다.
+    # 이 경우를 나머지로 더해주는 것으로 짐작된다.
+    m = 1 + min(dp(n // 2) + n % 2, dp(n // 3) + n % 3)
+    memo[n] = m
+    return m
+
+
+memo = {1: 0, 2: 1}
+N = int(input())
+print(dp(N))
