66806ade80f7e5a8ee90549b02e83a6b6a0037ef
dohee479 <dohee479@naver.com>
2021-01-24 20:10:42 +0900

    요세푸스 문제 0

--- 큐/11866.py
+++ 큐/11866.py
@@ -2,7 +2,6 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-# 실패
 N, K = map(int, sys.stdin.readline().rstrip().split())
 yose = [i for i in range(1, N + 1)]
 adj = [0] * N
@@ -10,15 +9,27 @@ result = []
 front = 0
 cnt = 1
 while len(result) != N:
+    # 인덱스의 길이를 넘어갈 시 0으로 가게 하기 위해서
     if front >= N:
         front %= N
     if yose[front]:
-        if not cnt % 3:
+        if not cnt % K:
             result.append(yose[front])
             yose[front] = 0
+    # 0 이면 카운트 되지 않으므로 -1을 해준다.
     else:
         cnt -= 1
     front += 1
     cnt += 1
 print(str(result).replace('[', '<').replace(']', '>'))
 
+# 내가 시도하려고 했던 방식
+N, K = map(int, input().split())
+l = list(range(1, N+1))
+p = list()
+i = 0
+while l:
+    i = (i + K - 1) % len(l)
+    p.append(str(l.pop(i)))
+
+print('<'+', '.join(p)+'>')
