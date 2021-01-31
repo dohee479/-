52c77d07e86a2482b10e76e88405af40569d4d34
dohee479 <dohee479@naver.com>
2021-01-31 19:44:53 +0900

    최대공약수와 최소공배수

---
+++ 정수론 및 조합론/2609.py
@@ -0,0 +1,21 @@
+# 최대공약수와 최소공배수
+import sys
+sys.stdin = open('input.txt', 'r')
+
+
+# 최대공약수를 구하는 방법인 유클리드호제법
+def euclid(n1, n2):
+    if n2 > n1:
+        n1, n2 = n2, n1
+    # 큰수를 작은 수로 나누어 나머지가 0이 될 때 작은수가 최대공약수
+    while n2:
+        n1, n2 = n2, n1 % n2
+    return n1
+
+
+num1, num2 = map(int, sys.stdin.readline().rstrip().split())
+gcd = euclid(num1, num2)
+print(gcd)
+# 두 수를 곱한 후 최대공약수로 나누면 최소공배수
+print(num1 * num2 // gcd)
+
