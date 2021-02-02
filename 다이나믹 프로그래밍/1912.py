1a951882e2e9f4d2f3e243b8a0ae883a5cd70ec3
dohee479 <dohee479@naver.com>
2021-02-02 16:39:06 +0900

    연속합

---
+++ 다이나믹 프로그래밍/1912.py
@@ -0,0 +1,13 @@
+# 연속합
+import sys
+sys.stdin = open('input.txt', 'r')
+
+# 시간초과
+n = int(input())
+seq = list(map(int, sys.stdin.readline().rstrip().split()))
+memo = [0] * n
+memo[0] = seq[0]
+for i in range(n):
+    for j in range(i + 1, n):
+        memo[j] = max(memo[j - 1] + seq[j], memo[j])
+print(max(memo))
