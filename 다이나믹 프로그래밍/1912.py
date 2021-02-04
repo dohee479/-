583fbbcb822c8f03ab1029a94268ee10b75322e7
dohee479 <dohee479@naver.com>
2021-02-04 16:09:35 +0900

    연속합

--- 다이나믹 프로그래밍/1912.py
+++ 다이나믹 프로그래밍/1912.py
@@ -6,7 +6,7 @@ n = int(input())
 seq = list(map(int, sys.stdin.readline().rstrip().split()))
 memo = [0] * n
 memo[0] = seq[0]
-for i in range(n):
-    for j in range(i + 1, n):
-        memo[j] = max(memo[j - 1] + seq[j], memo[j])
+# i번째서의 최대값은 i - 1까지의 최대값에 i를 더해준 값이다.
+for i in range(1, n):
+    memo[i] = max(memo[i - 1] + seq[i], seq[i])
 print(max(memo))
