85d38b0d774c97a1ff7fb7d76de437ef8b94eb41
dohee479 <dohee479@naver.com>
2021-02-14 19:38:45 +0900

    팩토리얼 0의 개수

--- 정수론 및 조합론/1676.py
+++ 정수론 및 조합론/1676.py
@@ -18,3 +18,21 @@ for i in range(len(number) - 1, -1, -1):
         print(answer)
         break
     answer += 1
+
+
+# 참조
+# 팩토리얼에서 0이 늘어나는 순간은 10(2 * 5)이 곱해지는 순간이다.
+# 따라서, 5의 개수를 찾으면 된다.
+# 10! 일 경우 5에서 한번, 10에서 한번 총 2번이다.
+n = int(input())
+arr = [0 for i in range(n+1)]
+for i in range(1,n+1):
+    arr[i] = arr[i-1]
+    idx = i
+    if i%5 == 0:
+        t = 0
+        while i % 5 == 0:
+            i //=5
+            t+=1
+        arr[idx] += t
+print(arr[n])
