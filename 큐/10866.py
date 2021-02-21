c19181e0e4934a1de35f14604fdc3584e392c377
dohee479 <dohee479@naver.com>
2021-02-21 18:55:15 +0900

    덱

--- 큐/10866.py
+++ 큐/10866.py
@@ -23,12 +23,13 @@ for _ in range(N):
     elif cmd[0] == 'pop_front':
         if rear >= front:
             print(dequeue[front])
-            dequeue[front:front + 1] = []
-            rear -= 1
+            # 빼주는 것을 넣을 경우 O(N)이다.
+            # dequeue[front:front + 1] = []
+            # rear -= 1
             # dequeue[front:front + 1] = [] 넣을 시 밑에는 필요없고 rear -= 1는 필요
-            # front += 1
-            # if front > rear:
-            #     rear = front - 1
+            front += 1
+            if front > rear:
+                rear = front - 1
         else:
             print(-1)
 
