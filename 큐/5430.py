363b850abd4e629d9eb4cc981b64775e94e5340b
dohee479 <dohee3959@gmail.com>
2021-02-25 23:35:18 +0900

    AC

--- 큐/5430.py
+++ 큐/5430.py
@@ -6,16 +6,26 @@ sys.stdin = open('input.txt', 'r')
 
 
 # 슬라이싱 + pop(0) 시간초과, reverse + popleft 시간초과
-def AC(command, x):
+def AC(command):
+    flip = False
     for cmd in command:
+        # R 명령 : 배열을 진짜 뒤집으면 안된다. 원소의 순서를 바꾸면 원소 수만큼 시간이 걸린다.
+        # 뒤집지 않고 구현 효과를 내야한다. [::-1] 사용 하면 안된다.
         if cmd == 'R':
-            x = deque(sorted(x, reverse=True))
+            flip = not flip
         elif cmd == 'D':
             if x:
-                x.popleft()
+                # list.pop() 역시 원소의 수에 비례하여 소요, deque 처리
+                if flip:
+                    x.pop()
+                else:
+                    x.popleft()
             else:
                 return 'error'
-    return x
+    if not flip:
+        return "[" + ','.join(x) + "]"
+    else:
+        return "[" + ','.join(reversed(x)) + "]"
 
 
 T = int(input())
@@ -24,7 +34,10 @@ for t in range(T):
     p = sys.stdin.readline().rstrip()
     n = sys.stdin.readline().rstrip()
     x = deque(r.findall(sys.stdin.readline().rstrip()))
-    print(AC(p, x))
+    print(AC(p))
+    
+    
+
 
 
 
