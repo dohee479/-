77ed46da7e0de17d7f3ec9faf5b1bb1efa7b9d6b
dohee479 <dohee3959@gmail.com>
2021-03-02 23:54:38 +0900

    오큰수

--- 스택/17298.py
+++ 스택/17298.py
@@ -5,21 +5,20 @@ sys.stdin = open('input.txt', 'r')
 
 def right_big():
     stack = []
-    answer = [0] * N
+    answer = [-1] * N
     for i, val in enumerate(A):
         if i == N - 1:
             answer[i] = -1
         else:
-            for j in range(len(stack) - 1, -1, -1):
-                if stack[j][1] < A[i + 1]:
-                    answer[stack[j][0]] = A[i + 1]
-                    stack.pop()
+            # 스택을 거꾸로 반복문을 돌리면 시간초과
+            # while을 사용하여 뒤에 들어가 있는 값만 비교하면 통과
+            while stack and stack[-1][1] < A[i + 1]:
+                answer[stack[-1][0]] = A[i + 1]
+                stack.pop()
             if A[i] < A[i + 1]:
                 answer[i] = A[i + 1]
             else:
                 stack.append((i, val))
-    for i, val in stack:
-        answer[i] = -1
     return ' '.join(map(str, answer))
 
 
